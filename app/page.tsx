12730267023df1df36c7d0c463ae1ebdab7e1d2b
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-01-12 01:09:01 +0600

    fix: next font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Fira_Code } from '@next/font/google'
+
+const font = Fira_Code({
+    weight: '700'
+})
+
 export default function Home() {
     return (
-        <main>
-            Home page
+        <main className={font.className}>
+            Главная
         </main>
     )
 }
