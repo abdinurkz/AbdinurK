881037172dea1e306833a5cb4f4362941a4baed5
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-01-11 23:58:43 +0600

    fix: main

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+ return (
+    <>
+      <title>Kuatbek Abdinur - Frontend Developer</title>
+      <meta name="description" content="JavaScript enthusiast." />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
