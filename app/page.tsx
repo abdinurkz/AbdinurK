0d02fda4cf2f48cec4202764edbc16437557b218
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-01-12 21:40:41 +0600

    fix: main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+import styles from './styles.module.scss'
+import Image from 'next/image'
+
+
 export default function Home() {
     return (
-        <main>
-            Мен туралы
+        <main className={styles['page-wrapper']}>
+            <section className={styles['page-content']}>
+                <div className={styles['info']}>
+                    <div className={styles['details']}>
+                        <h1 className={styles['full-name']}>
+                            Қуатбек Әбдінұр
+                        </h1>
+                        <h2 className={styles['position']}>
+                            Орташа фронтенд әзірлеуші
+                        </h2>
+                        <p className={styles['description']}>
+
+                        </p>
+                    </div>
+                    <figure>
+                        <Image 
+                            src="/avatar.png" 
+                            alt="avatar" 
+                            width={116} 
+                            height={116}
+                        />
+                    </figure>
+                </div>
+            </section>
         </main>
     )
 }
