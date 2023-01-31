a5d0903edea929ef9b16033c884f334868fd5abd
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-01-31 20:36:11 +0600

    fix: linkedin

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -10,6 +10,7 @@ export default function Contacts() {
                 <p>Telegram: <Link href="https://t.me/abdinur08" >@abdinur08</Link></p>
                 <p>Github: <Link href="https://github.com/AbdinurK">AbdinurK</Link></p>
                 <p>Gmail: <Link href="mailto:kuatbekabdinur@gmail.com">kuatbekabdinur@gmail.com</Link></p>
+                <p>LinkedIn: <Link href="https://www.linkedin.com/in/abdinur-kuatbek-80241b190/">Abdinur Kuatbek</Link></p>
             </section>
         </main>
     ) 
