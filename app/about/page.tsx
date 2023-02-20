9bf5f127373847888e307232a497f4b43834d416
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-02-21 00:03:06 +0600

    fix: almost done

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,18 +1,15 @@
-import styles from './styles.module.scss'
 import Link from 'next/link'
 
 
 export default function About() {
   return (
-    <main className={styles['page-wrapper']}>
-      <main className={styles['page-wrapper']}>
-        <section className={styles['page-content']}>
-          <p>Telegram: <Link href="https://t.me/abdinur08" >@abdinur08</Link></p>
-          <p>Github: <Link href="https://github.com/AbdinurK">AbdinurK</Link></p>
-          <p>Gmail: <Link href="mailto:kuatbekabdinur@gmail.com">kuatbekabdinur@gmail.com</Link></p>
-          <p>LinkedIn: <Link href="https://www.linkedin.com/in/abdinur-kuatbek-80241b190/">Abdinur Kuatbek</Link></p>
-        </section>
-      </main>
+    <main className="max-w-2xl w-full flex justify-between items-center mx-auto">
+      <section className="pl-[20px]">
+        <p>Telegram: <Link href="https://t.me/abdinur08" >@abdinur08</Link></p>
+        <p>Github: <Link href="https://github.com/AbdinurK">AbdinurK</Link></p>
+        <p>Gmail: <Link href="mailto:kuatbekabdinur@gmail.com">kuatbekabdinur@gmail.com</Link></p>
+        <p>LinkedIn: <Link href="https://www.linkedin.com/in/abdinur-kuatbek-80241b190/">Abdinur Kuatbek</Link></p>
+      </section>
     </main>
   )
 }
---
+++ components/ThemeSwitch/index.tsx
@@ -0,0 +1,6 @@
+import { ThemeSwitch } from './ThemeSwitch'
+
+
+export {
+  ThemeSwitch
+}
