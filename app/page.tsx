30015b0052be15a6e452fa0467d4cd4eb864554a
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-02-27 23:18:29 +0600

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -18,24 +18,7 @@ export default function Home() {
                 Middle front-end developer
               </h2>
             </Up>
-            <Up delay={0.4}>
-              <p className="text-[15px] mb-3 leading-6">
-                Front-end developer with 2.5 years of commercial experience. Skills:
-              </p>
-              <ul className="list-disc pl-0 m-0">
-                <li>HTML, CSS, SASS, ES6+.</li>
-                <li>React, Next, Typescript, Redux Toolkit / Query, Antd design, Eslint, Webpack.</li>
-                <li>I am able to complete tasks on time.</li>
-                <li>Easy to adapt and learn.</li>
-                <li>Ability to read code, also writing understandable code using DRY, KISS, SOLID programming principles.</li>
-                <li>Communication and tolerance.</li>
-              </ul>
-            </Up>
-            <Up delay={0.6}>
-              <p className='mt-1'>
-                I will be glad to cooperate! Also open to new suggestions.
-              </p>
-            </Up>
+            
           </div>
           <figure>
             <Up delay={0.1}> 
@@ -50,6 +33,21 @@ export default function Home() {
             </Up>
           </figure>
         </div>
+        <Up delay={0.4}>
+          <p className="mb-3">
+            Front-end developer with 2.5 years of commercial experience.Knowledge: React.js, Vue.js, Next.js, Typescript, TailwindCSS,
+            Redux Toolkit, Pinia, Webpack, Vite, Eslint, Prettier. 
+            Also, I have:
+          </p>
+          <ul className="list-disc pl-0 m-0">
+            <li>Ability writing clean, efficient and maintainable code using DRY, KISS, SOLID programming principles.</li>
+            <li>Experience of implementing responsive designs that work flawlessly on desktop and mobile devices.</li>
+            <li>Strong problem-solving skills and attention to details.</li>
+          </ul>
+          <p className='mt-3'>
+            I will be glad to cooperate and open to new suggestions!
+          </p>
+        </Up>
       </section>
     </main>
   )
