e0361276977d453820db3516b4bb444fc6186a75
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-02-28 22:20:57 +0600

    fix

---
+++ app/projects/error.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useEffect } from 'react'
+
+
+export default function Error({
+  error,
+  // reset
+}: {
+  error: Error
+  reset: () => void
+}) {
+
+  useEffect(() => { 
+    console.error(error)
+  }, [error]);
+
+  // const handleRefreshPage = () => reset()
+
+  return (
+    <div className='flex items-center justify-center'>
+      <p>Oh no, something went wrong on projects page... maybe refresh?</p>
+    </div>
+  );
+}
