f4079d8b6736e5f9f05f87f2b2b7c2e14bce80a8
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-03-04 23:18:05 +0600

    fix

--- app/[lng]/cv/page.tsx
+++ app/[lng]/cv/page.tsx
@@ -1,14 +1,27 @@
 import data from 'content/cv.json'
+import { useTranslation } from '../../../i18n'
 import { JobCard } from 'components/JobCard'
 
 
-export default function CV() {
+export default async function CV({ params: { lng } }: {
+  params: {
+    lng: string;
+  };
+}) {
+
+  const { t } = await useTranslation(lng, 'cv')
   return (
     <main className="max-w-2xl w-full flex justify-between items-center mx-auto">
       <section className="px-[20px]">
         <div className="w-full">
           <h2 className="font-normal text-lg mt-0 mb-4 border-b border-black dark:border-white">
-            Language
+            Experience
+          </h2>
+          {
+            data.jobs.map((job) => <JobCard key={job.id} job={job} />)
+          }
+          <h2 className="font-normal text-lg mt-2 mb-4 border-b border-black dark:border-white">
+            { t('language') }
           </h2>
           {
             data.languages.map((lang) => (
@@ -16,12 +29,6 @@ export default function CV() {
                 <span>{ lang.text }</span> - <span>{ lang.level }</span>
               </article>
             ))
-          }
-          <h2 className="font-normal text-lg mt-2 mb-4 border-b border-black dark:border-white">
-            Experience
-          </h2>
-          {
-            data.jobs.map((job) => <JobCard key={job.id} job={job} />)
           }
         </div>
       </section>
