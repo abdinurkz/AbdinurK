838a828dfdc1136ed5951e7597608809bb7c3e3b
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-03-25 22:30:35 +0600

    fix: locale

--- app/[lng]/projects/page.tsx
+++ app/[lng]/projects/page.tsx
@@ -1,10 +1,15 @@
-import { List } from 'components/List'
+import { useTranslation } from 'internationalization'
 
-export default function Projects() {
+export default async function Projects({ params: { lng } }: {
+  params: {
+    lng: string;
+  };
+}) {
+  const { t } = await useTranslation(lng, 'projects')
   return (
     <main className="max-w-2xl w-full flex justify-between items-center mx-auto">
       <section className="pl-[20px]">
-        Coming soon...
+        { t('plug') }
       </section>
     </main>
   ) 
