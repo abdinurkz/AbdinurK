bb428e193d35dd6e2171f972dd744c1bbe5f2cdd
Abdinur Kuatbek <kuatbekabdinur@gmail.com>
2023-03-26 23:29:26 +0600

    fix: projects list

--- components/Icons/index.tsx
+++ components/Icons/index.tsx
@@ -173,5 +173,38 @@ export function TwitterIcon(props: ComponentProps<'svg'>): ReactElement {
   );
 }
 
+export function ExternalLinkIcon(props: ComponentProps<'svg'>): ReactElement {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      style={{ width: '1rem', height: '1rem', fill: 'currentColor' }}
+    >
+      <g data-name="Layer 2">
+        <g data-name="external-link">
+          <rect width="24" height="24" opacity="0"></rect>
+          <path d="M20 11a1 1 0 0 0-1 1v6a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h6a1 1 0 0 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1z"></path>
+          <path d="M16 5h1.58l-6.29 6.28a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0L19 6.42V8a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4a1 1 0 0 0-1-1h-4a1 1 0 0 0 0 2z"></path>
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+export function StarIcon(props: ComponentProps<'svg'>): ReactElement {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 16 16"
+      style={{ width: '1rem', height: '1rem', fill: 'currentColor' }}
+    >
+      <path
+        fillRule="evenodd"
+        d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
+      ></path>
+    </svg>
+  )
+}
+
 
 
--- components/List/index.tsx
+++ components/List/index.tsx
@@ -1,5 +1,7 @@
 'use client'
 import { useEffect, useState } from 'react'
+import { ExternalLinkIcon, StarIcon } from 'components/Icons'
+import Link from 'next/link'
 
 export function List() {
   const [repos, setRepos] = useState([])
@@ -23,7 +25,66 @@ export function List() {
   
   return (
     <div>
-
+      <section className="segment">
+        <div className="container">
+          <div className="flex flex-col w-full flex-grow">
+            {repos.slice(0, 5).map((project) => {
+              return (
+                <div className="card anchored large border-l-fuchsia-600 w-full" key={project.slug}>
+                  <div className="stars">
+                    <div className="star">
+                      <a
+                        href={`https://github.com/abdinurk/${project.slug}/stargazers`}
+                      >
+                        {Number(
+                          project.stargazers_count
+                        ).toLocaleString()}
+                      </a>
+                      <StarIcon />
+                    </div>
+                  </div>
+                  <div>
+                    <a
+                      className="card-header"
+                      href={`https://github.com/abdinurk/${project.slug}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      {project.name}
+                    </a>
+                    <p>{project.tagline}</p>
+                  </div>
+                  <div className="links anchored">
+                    {project.writeup && (
+                      <Link className="button small" href={project.writeup}>
+                        Article
+                      </Link>
+                    )}
+                    {/* {project.url && (
+                      <a
+                        className="button small flex"
+                        href={project.url}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Demo <ExternalLinkIcon />
+                      </a>
+                    )} */}
+                    <a
+                      className="button small flex"
+                      href={`https://github.com/abdinurk/${project.slug}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Source <ExternalLinkIcon />
+                    </a>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
     </div>
   ) 
 } 
